7ae45b8c62a5e05fcc12b1fffbfffe0550a5ee8b
ajaykushwaha <ajaykushwahaofficial@gmail.com>
2019-09-30 23:50:12 +0530

    Changes Related Pojo

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+    private String firstName;
+    private String lastNeme;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastNeme() {
+        return lastNeme;
+    }
+
+    public void setLastNeme(String lastNeme) {
+        this.lastNeme = lastNeme;
+    }
+}
