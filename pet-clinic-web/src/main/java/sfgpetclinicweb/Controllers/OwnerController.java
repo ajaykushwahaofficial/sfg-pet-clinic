bd5fa32ac895abfc58d35244b5da0478ba2994ad
ajaykushwaha <ajaykushwahaofficial@gmail.com>
2019-10-07 01:25:52 +0530

    Add owner page and Controller Closes#13

--- pet-clinic-web/src/main/java/sfgpetclinicweb/Controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/sfgpetclinicweb/Controllers/OwnerController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/vets")
-public class VetController {
+@RequestMapping("/owner")
+public class OwnerController {
 
     @RequestMapping({"","/","index","index.html"})
-    public String getListOfVets() {
-        return "vets/index";
+    public String getListOfOwner() {
+        return "owner/index";
     }
 }
