c858041a6676c0f3611333b4755d14e7202e050d
ajaykushwaha <ajaykushwahaofficial@gmail.com>
2019-10-14 00:42:12 +0530

    Added Vets list and updated controllers Closes#15

--- pet-clinic-web/src/main/java/sfgpetclinicweb/Controllers/VetController.java
+++ pet-clinic-web/src/main/java/sfgpetclinicweb/Controllers/VetController.java
@@ -1,14 +1,23 @@
 package sfgpetclinicweb.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sfgpetclinicweb.servicces.map.VetMapService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private VetMapService vetMapService;
+
+    public VetController(VetMapService vetMapService){
+        this.vetMapService=vetMapService;
+    }
+
     @RequestMapping({"","/","index","index.html"})
-    public String getListOfVets() {
+    public String getListOfVets(Model model) {
+        model.addAttribute("vets",vetMapService.findAll());
         return "vets/index";
     }
 }
