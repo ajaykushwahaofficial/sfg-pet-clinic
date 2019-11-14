7b14ca4311b5a1d6107f336e5159a7b3dc8c943d
ajaykushwaha <ajaykushwahaofficial@gmail.com>
2019-11-14 22:51:36 +0530

    Add Spring Data JPA repositories Closes #40

---
+++ pet-clinic-data/src/main/java/sfgpetclinicweb/repositories/OwnerRepository.java
@@ -0,0 +1,8 @@
+package sfgpetclinicweb.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import sfgpetclinicweb.model.Owner;
+
+public interface OwnerRepository extends CrudRepository<Owner,Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/sfgpetclinicweb/repositories/VetRepository.java
@@ -0,0 +1,7 @@
+package sfgpetclinicweb.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import sfgpetclinicweb.model.Vet;
+
+public interface VetRepository extends CrudRepository<Vet,Long> {
+}
