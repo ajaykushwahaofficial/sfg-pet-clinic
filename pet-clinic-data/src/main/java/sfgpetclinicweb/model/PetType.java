0866d46dd14f9be70618f5196c4fc76bef0be70f
ajaykushwahaofficial <ajaykushwahaofficial@gmail.com>
2020-09-22 00:01:03 +0530

    builder pattern to PetType. Closes #58

--- pet-clinic-data/src/main/java/sfgpetclinicweb/model/PetType.java
+++ pet-clinic-data/src/main/java/sfgpetclinicweb/model/PetType.java
@@ -1,10 +1,14 @@
 package sfgpetclinicweb.model;
 
-import lombok.*;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Setter
 @Getter
@@ -22,4 +26,9 @@ public class PetType extends BaseEntity {
 	@Column(name = "name")
     private String name;
 
+	@Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
 }
