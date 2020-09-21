d1b428d30c8814eb3b1ca6b7792d59a4b56e2923
ajaykushwahaofficial <ajaykushwahaofficial@gmail.com>
2020-09-21 23:58:26 +0530

    Add ability to add or update a Pet Closes #58

---
+++ pet-clinic-web/src/main/java/sfgpetclinicweb/Controllers/PetController.java
@@ -0,0 +1,48 @@
+package sfgpetclinicweb.Controllers;
+
+import java.util.Collection;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import sfgpetclinicweb.model.Owner;
+import sfgpetclinicweb.model.PetType;
+import sfgpetclinicweb.services.OwnerService;
+import sfgpetclinicweb.services.PetService;
+import sfgpetclinicweb.services.PetTypeService;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
